4c232ca889ec15a09b99b9b248a197224a2e0456
bitsheep <dev@bitsheep.net>
2025-02-22 19:10:09 +0100

    Added pydantic models for jwt access token

--- backend/app/schemas/oidc.py
+++ backend/app/schemas/oidc.py
@@ -1,4 +1,7 @@
+from datetime import datetime
 from pydantic import BaseModel, Field
+from typing import Optional
+from uuid import UUID
 
 class AuthorizationResponse(BaseModel):
     state: str = Field(description="OAuth state")
@@ -8,3 +11,68 @@ class AuthorizationResponse(BaseModel):
     # OAuth2 Issuer as per https://datatracker.ietf.org/doc/html/rfc9207
     iss: str = Field(description="Identity of the Authorization Server")
     code: str = Field(description="OAuth Authorization Code Flow code")
+
+
+class RealmAccess(BaseModel):
+    roles: list[str] = Field(description="User roles in the Keycloak realm")
+    
+
+class ResourceRoles(BaseModel):
+    roles: list[str] = Field(description="User roles")
+
+
+class AccessTokenClaims(BaseModel):
+    allowed_origins: Optional[list[str]] = Field(
+        default=None,
+        description="Optional: Allowed request origins (i think?)"
+    )
+    aud: list[str] = Field(description="Required: Intended JWT recipients")
+    auth_time: Optional[datetime] = Field(
+        default=None,
+        description="Optional: Time when the End-User authentication occurred"
+    )
+    azp: Optional[str] = Field(
+        default=None,
+        description="Optional: Party the token was issued to"
+    )
+    email: Optional[str] = Field(
+        default=None,
+        description="Optional: E-Mail address of the subject"
+    )
+    email_verified: Optional[bool] = Field(
+        default=None,
+        description="Optional: E-Mail verification status of the subject in Keycloak"
+    )
+    exp: datetime = Field(description="Required: Expiry time of token")
+    family_name: Optional[str] = Field(
+        default=None,
+        description="Optional: Family name of the subject"
+    )
+    given_name: Optional[str] = Field(
+        default=None,
+        description="Optional: Given name of the subject"
+    )
+    iat: datetime = Field(description="Required: Time of token issuance")
+    jti: UUID = Field(description="Required: Unique JWT identifier")
+    name: Optional[str] = Field(
+        default=None,
+        description="Optional: Full name of the subject"
+    )
+    preferred_username: Optional[str] = Field(
+        default=None,
+        description="Optional: Preferred username of the subject"
+    )
+    realm_access: Optional[RealmAccess] = Field(
+        default=None,
+        description="Optional: User roles in the Keycloak realm"
+    )
+    resource_access: Optional[dict[str, "ResourceRoles"]] = Field(
+        default=None,
+        description="Optional: User specific access roles per resource as defined in Keycloak"
+    )
+    scope: str = Field(description="Required: Requested OAuth scopes")
+    sub: UUID = Field(description="Required: Subject Identifier")
+    typ: Optional[str] = Field(
+        default=None,
+        description="Optional: Token type"
+    )
